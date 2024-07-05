830587022dad9f058a36c4f5ed4ad1d6790ca08f
abdul <khal444d@gmail.com>
2024-07-05 23:39:41 +0100

    remove comma before 'as'

--- abdul_demo_dbt/models/Dev_DataOps_Transformation/table_join_model_V.sql
+++ abdul_demo_dbt/models/Dev_DataOps_Transformation/table_join_model_V.sql
@@ -1,4 +1,4 @@
-SELECT distinct meets.meetid, meets.MeetTown, pl.Name, as sport_person_name pl.age 
+SELECT distinct meets.meetid, meets.MeetTown, pl.Name as sport_person_name pl.age 
 
 FROM {{ source("data_ingestion","meets") }}meets left join {{ source("data_ingestion","powerlifting") }}pl 
 
