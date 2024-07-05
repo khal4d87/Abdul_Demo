9518757094022838085277dbc287b8bf99d6c857
abdul <khal444d@gmail.com>
2024-07-06 00:48:16 +0100

    correcting code

--- abdul_demo_dbt/models/Dev_DataOps_Transformation/table_join_model_V.sql
+++ abdul_demo_dbt/models/Dev_DataOps_Transformation/table_join_model_V.sql
@@ -1,5 +1,4 @@
-SELECT distinct meets.meetid, meets.MeetTown, pl.Name as sport_person_name pl.age 
-
-FROM {{ source("data_ingestion","meets") }}meets left join {{ source("data_ingestion","powerlifting") }}pl 
-
-on meets.MeetID=pl.MeetID  where pl.MeetID=5 
+SELECT DISTINCT meets.meetid, meets.MeetTown, pl.Name AS sport_person_name, pl.age
+FROM {{ source("data_ingestion", "meets") }} meets
+LEFT JOIN {{ source("data_ingestion", "powerlifting") }} pl
+ON meets.MeetID = pl.MeetID AND pl.MeetID = 5;
