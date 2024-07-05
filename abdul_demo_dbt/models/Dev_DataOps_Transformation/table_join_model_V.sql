8e241262a385949264c13f371dcf636c31089eff
abdul <khal444d@gmail.com>
2024-07-05 23:36:06 +0100

    added model and table join in transformation layer. renamed name column to sports_person_name

---
+++ abdul_demo_dbt/models/Dev_DataOps_Transformation/table_join_model_V.sql
@@ -0,0 +1,5 @@
+SELECT distinct meets.meetid, meets.MeetTown, pl.Name, as sport_person_name pl.age 
+
+FROM {{ source("data_ingestion","meets") }}meets left join {{ source("data_ingestion","powerlifting") }}pl 
+
+on meets.MeetID=pl.MeetID  where pl.MeetID=5 
