85bd28216ae9de93c571805b622cb67c2cebfa75
abdul <khal444d@gmail.com>
2024-07-13 12:47:35 +0100

    correcting

--- abdul_demo_dbt/models/Dev_Data_Ops_Raw/US_UK_names_edited_V.sql
+++ abdul_demo_dbt/models/Dev_Data_Ops_Raw/US_UK_names_edited_V.sql
@@ -2,7 +2,7 @@ SELECT *,
 Case
     when MeetCountry = 'UK' Then 'United_Kingdom'
     when MeetCountry = 'US' Then 'United_States'
-    Else Unknown
+    Else MeetCountry
 End
 FROM  
 {{ source("data_ingestion","meets") }}  
