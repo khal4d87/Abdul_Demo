2b83dc0960830d00db091d7e2fb3006b905a1698
abdul <khal444d@gmail.com>
2024-07-13 13:31:56 +0100

    changing us to usa

--- abdul_demo_dbt/models/Dev_Data_Ops_Raw/US_UK_names_edited_V.sql
+++ abdul_demo_dbt/models/Dev_Data_Ops_Raw/US_UK_names_edited_V.sql
@@ -1,7 +1,7 @@
 SELECT *,
 Case
     when MeetCountry = 'UK' Then 'United_Kingdom'
-    when MeetCountry = 'US' Then 'United_States'
+    when MeetCountry = 'USA' Then 'United_States'
     Else MeetCountry
 End AS FullCountryName
 FROM  
