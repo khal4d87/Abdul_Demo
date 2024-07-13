9f123b84bd14bd32f617fda762c5707cd2c39b8a
abdul <khal444d@gmail.com>
2024-07-13 12:38:35 +0100

    Change uk and us entries

--- abdul_demo_dbt/models/Dev_Data_Ops_Raw/US_UK_names_edited_V.sql
+++ abdul_demo_dbt/models/Dev_Data_Ops_Raw/US_UK_names_edited_V.sql
@@ -1,3 +1,8 @@
 SELECT *
+Case
+    when MeetCountry = 'UK' Then 'United_Kingdom'
+    when MeetCountry = 'US' Then 'United_States'
+    Else Unknown
+End
 FROM  
 {{ source("data_ingestion","meets") }}  
